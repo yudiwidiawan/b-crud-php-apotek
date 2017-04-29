d2bcbd745632cab3926a314c93817a98732c4825
falah12313 <khalifahfalahh@gmail.com>
2017-04-29 11:46:55 +0700

    database doang

---
+++ database.sql
@@ -0,0 +1,50 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.21-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping database structure for apotek
+CREATE DATABASE IF NOT EXISTS `apotek` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `apotek`;
+
+
+-- Dumping structure for table apotek.obat
+CREATE TABLE IF NOT EXISTS `obat` (
+  `kode_obat` varchar(5) NOT NULL,
+  `nama_obat` varchar(30) NOT NULL,
+  `jenis` varchar(20) NOT NULL,
+  `harga` float NOT NULL,
+  `kd_supplier` varchar(5) NOT NULL,
+  PRIMARY KEY (`kode_obat`),
+  KEY `FK_obat_supplier` (`kd_supplier`),
+  CONSTRAINT `FK_obat_supplier` FOREIGN KEY (`kd_supplier`) REFERENCES `supplier` (`kd_supplier`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table apotek.obat: ~0 rows (approximately)
+/*!40000 ALTER TABLE `obat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `obat` ENABLE KEYS */;
+
+
+-- Dumping structure for table apotek.supplier
+CREATE TABLE IF NOT EXISTS `supplier` (
+  `kd_supplier` varchar(5) NOT NULL,
+  `nama_supplier` varchar(50) NOT NULL,
+  `no_telp` varchar(15) NOT NULL,
+  `alamat_kantor` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`kd_supplier`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table apotek.supplier: ~0 rows (approximately)
+/*!40000 ALTER TABLE `supplier` DISABLE KEYS */;
+/*!40000 ALTER TABLE `supplier` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
