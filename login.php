c2f25db2c9f6dc9196ac9e44af961954f3aac031
Alfianur <alfianurmartadipura@yahoo.com>
2017-04-29 12:06:48 +0700

    Mengganti Login

--- login.php
+++ login.php
@@ -10,24 +10,24 @@
 include("connection.php");
 
 if(isset($_POST['submit'])) {
-	$user = mysqli_real_escape_string($mysqli, $_POST['username']);
+	$kode_supplier = mysqli_real_escape_string($mysqli, $_POST['kode_supplier']);
 	$pass = mysqli_real_escape_string($mysqli, $_POST['password']);
 
-	if($user == "" || $pass == "") {
-		echo "Either username or password field is empty.";
+	if($kode_supplier == "" || $pass == "") {
+		echo "Either kode_supplier or password field is empty.";
 		echo "<br/>";
 		echo "<a href='login.php'>Go back</a>";
 	} else {
-		$result = mysqli_query($mysqli, "SELECT * FROM login WHERE username='$user' AND password=md5('$pass')")
+		$result = mysqli_query($mysqli, "SELECT * FROM supplier WHERE kd_supplier='$kode_supplier' AND password=md5('$pass')")
 					or die("Could not execute the select query.");
 		
 		$row = mysqli_fetch_assoc($result);
 		
 		if(is_array($row) && !empty($row)) {
-			$validuser = $row['username'];
+			$validuser = $row['kode_supplier'];
 			$_SESSION['valid'] = $validuser;
-			$_SESSION['name'] = $row['name'];
-			$_SESSION['id'] = $row['id'];
+			$_SESSION['nama_supplier'] = $row['nama_supplier'];
+			$_SESSION['kd_supplier'] = $row['kd_supplier'];
 		} else {
 			echo "Invalid username or password.";
 			echo "<br/>";
@@ -44,8 +44,8 @@ if(isset($_POST['submit'])) {
 	<form name="form1" method="post" action="">
 		<table width="75%" border="0">
 			<tr> 
-				<td width="10%">Username</td>
-				<td><input type="text" name="username"></td>
+				<td width="10%">kode_supplier</td>
+				<td><input type="text" name="kode_supplier"></td>
 			</tr>
 			<tr> 
 				<td>Password</td>
